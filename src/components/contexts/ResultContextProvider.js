ce028cf1590d5d963e9f945a5ab49235bf02cd3f
Enz000 <enzosantamaria@outlook.fr>
2021-12-27 16:44:18 +0100

    usecontext & Api

---
+++ src/components/contexts/ResultContextProvider.js
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultcontextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState(
+    "Pain au chocolat ou chocolatine ? "
+  );
+
+  const getResults = async (type) => {
+    setIsLoading(true);
+
+    const response = await fetch(`${baseUrl}${type}`, {
+      method: "GET",
+      headers: {
+        "x-user-agent": "desktop",
+        "x-proxy-location": "FR",
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+      },
+    });
+    const data = await response.json();
+
+    setResults(data);
+    setIsLoading(false);
+  };
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
